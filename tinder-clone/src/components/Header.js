f1a94a635c285e3d2ca02cc8dbe85920feb041b9
Sthabiso <sthabiso@aims.ac.za>
2023-11-06 23:15:35 +0200

    Added message icon

--- tinder-clone/src/components/Header.js
+++ tinder-clone/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PersonIcon from '@mui/icons-material/Person';
-
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 function Header(){
     return(
         <div>
             <PersonIcon />
             <h2>I am a header</h2>
+            <QuestionAnswerIcon />
         </div>
     )
 }
