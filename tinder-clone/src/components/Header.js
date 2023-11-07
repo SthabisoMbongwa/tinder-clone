22fe05c45067fe36626c42d9d668227d5856efea
Sthabiso <sthabiso@aims.ac.za>
2023-11-07 21:20:16 +0200

    Added IconButton for icons

--- tinder-clone/src/components/Header.js
+++ tinder-clone/src/components/Header.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "./Header.css";
-import tinder from '../images/tinder.png'
+import tinder from "../images/tinder.png";
 import PersonIcon from "@mui/icons-material/Person";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import { IconButton } from "@mui/material";
 
 function Header() {
   return (
     <div className="header">
-
-      <PersonIcon fontSize="large"/>
-
-      <img
-        className="header__logo"
-        src={tinder}
-        alt="tinder logo"
-      />
-
-      <QuestionAnswerIcon fontSize="large"/>
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <img className="header__logo" src={tinder} alt="tinder logo" />
+      <IconButton>
+        <QuestionAnswerIcon className="header__icon" fontSize="large" />
+      </IconButton>
     </div>
   );
 }
