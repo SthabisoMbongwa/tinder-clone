3596e1da542992beb744af7fe4a9c3566a236c12
Sthabiso <sthabiso@aims.ac.za>
2023-11-08 22:37:24 +0200

    Created home and chat components

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
      {/* Header */}
      <Header />
+     <Routes>
+      <Route path='/'><h1>I am the chat page</h1></Route>
+
+      <Route path='/chat'><h1>I am homepage</h1></Route>
+     </Routes>
      {/* Tinder Cards */}
      {/* Buttons below tinder cards */}
 
